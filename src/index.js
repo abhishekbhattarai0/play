1667b9c4d8482e66f6d372711aff89e2bb5b27fb
abhishekbhattarai0 <abhishekbhattarai831@gmail.com>
2024-06-04 12:56:54 +0545

    setup ApiError, ApiResponse and AsyncHandler

--- src/index.js
+++ src/index.js
@@ -1,11 +1,27 @@
 import { configDotenv } from 'dotenv';
 import connectDB from './db/index.js';
+import { app } from './app.js';
 
 configDotenv({path: './env'})
 
 
 connectDB()
+.then( () => {
+    app.on( "error",(error)=> {
+        console.log("ERROR : ", error);
+        throw error;
+    })
+    app.listen(process.env.PORT || 4000, ()=> {
+        console.log(`Server is running at http://localhost:${process.env.PORT || 4000 }`);
+    })
+})
+.catch( (error)=> {
+    console.log("Mongo Db connection failed :", error);
+})
 
+app.get("/", (req, res) => {
+    res.send("<h1> Finally Server is running</h1>")
+})
 
 
 
