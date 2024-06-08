f1d891f14ab86216a795144e7becac77deccf64d
abhishekbhattarai0 <abhishekbhattarai831@gmail.com>
2024-06-08 11:37:35 +0545

    add getWatchHistory endpoint

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -411,6 +411,7 @@ const getUserChannelProfile = asyncHandler( async( req, res) => {
    if (!channel?.length) {
       throw new ApiError(404, "Channel does not exist")
    }
+   console.log(" CHANNEL => ",channel)
 
    return res
    .status(200)
@@ -418,9 +419,62 @@ const getUserChannelProfile = asyncHandler( async( req, res) => {
       new ApiResponse(200, channel[0], "User channel fetched successively")
    )
 
-   console.log(" CHANNEL => ",channel)
 
 
+})
+
+const getWatchHistory = asyncHandler( async(req, res) => {
+   const user = await User.aggregate([
+      {
+         $match: {
+            _id: new mongoose.Types.ObjectId(req.user._id)
+         }
+      },
+      {
+         $lookup: {
+            from: "videos",
+            localField: "watchHistory",
+            foreignField: "_id",
+            as: "watchHistory",
+            pipeline: [
+               {
+                  $lookup: {
+                     from: "users",
+                     localField: "owner",
+                     foreignField: "_id",
+                     as: "owner",
+                     pipeline: [
+                        {
+                           $project: {
+                              fullname: 1,
+                              username: 1,
+                              avatar: 1
+                           }
+                        },
+                        {
+                           $addFields: {
+                              owner: {
+                                 $first: "$owner"
+                              }
+                           }
+                        }
+                     ]
+                  }
+               }
+            ]
+         }
+      }
+   ])
+
+   return res
+   .status(200)
+   .json(
+      new ApiResponse(
+         200,
+         user[0].watchHistory,
+         "Watch history"
+      )
+   )
 })
 
 export { 
@@ -433,6 +487,7 @@ export {
    updateAccountDetails,
    updateUserAvatar,
    updateUserCoverImage,
-   getUserChannelProfile
+   getUserChannelProfile,
+   getWatchHistory
 
 }
