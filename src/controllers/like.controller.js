496518ebef27ce82c1013f68f40fa68760937541
abhishekbhattarai0 <abhishekbhattarai831@gmail.com>
2024-06-14 12:54:02 +0545

    toggle video, comment, tweet like endpoint created and add route for them

--- src/controllers/like.controller.js
+++ src/controllers/like.controller.js
@@ -5,11 +5,12 @@ import {ApiResponse} from "../utils/ApiResponse.js"
 import {asyncHandler} from "../utils/asyncHandler.js"
 
 const toggleVideoLike = asyncHandler(async (req, res) => {
+    // video comment tweet likedBy 
     const {videoId} = req.params
     const userId = req.user?._id
-    const {like} = req.body
+   
     // TODO: toggle like on video
-    if( !userId && videoId) {
+    if( !userId && !videoId) {
         throw new ApiError(400, "video id and user id are required")
     }
     if (!isValidObjectId(videoId)){
@@ -20,15 +21,19 @@ const toggleVideoLike = asyncHandler(async (req, res) => {
         throw new ApiError(400, "Invalid User id")
     }
 
-    if(like){
+    const existedLike = await Like.findOne({video:videoId})
+
+    if(existedLike){
+        const unlikeVideo = await Like.findOneAndDelete(videoId)
+        return res.json( new ApiResponse(200, unlikeVideo, "You unliked a video"))
+       
+        
+    }else{
         const likeVideo = await Like.create({
             video: Object(videoId),
             likedBy: userId
         })
         return res.json( new ApiResponse(200, likeVideo, "You liked a video"))
-    }else{
-        const unlikeVideo = await Like.findByIdAndDelete(videoId)
-        return res.json( new ApiResponse(200, unlikeVideo, "You liked a video"))
     }
 
 
@@ -39,12 +44,75 @@ const toggleVideoLike = asyncHandler(async (req, res) => {
 const toggleCommentLike = asyncHandler(async (req, res) => {
     const {commentId} = req.params
     //TODO: toggle like on comment
+    const userId = req.user?._id
+    console.log(" user ID \n\n\n",userId)
 
+    if( !commentId && !userId){
+        throw new ApiError(400, "Require comment id and user id")
+    }
+
+    if (!isValidObjectId(commentId)){
+        throw new ApiError(400, "Invalid video Id")
+    }
+
+    if( !isValidObjectId(userId)){
+        throw new ApiError(400, "Invalid User id")
+    }
+
+    const existedLike = await Like.findOne({comment: commentId})
+
+    if( existedLike) {
+        const like = await Like.findOneAndDelete(commentId);
+
+        return res
+        .status(200)
+        .json( new ApiResponse(200, like, " Comment unliked"))
+    }else{
+        const like = await Like.create({
+            comment: commentId,
+            likedBy: userId
+        })
+
+        return res
+        .status(200)
+        .json( new ApiResponse(200, like, "Comment Liked"))
+    }
 })
 
 const toggleTweetLike = asyncHandler(async (req, res) => {
     const {tweetId} = req.params
     //TODO: toggle like on tweet
+    const userId = req.user?._id
+
+    if( !tweetId && !userId){
+        throw new ApiError(400, "Require tweetId and user id")
+    }
+
+    if (!isValidObjectId(tweetId)){
+        throw new ApiError(400, "Invalid tweetId")
+    }
+
+    if( !isValidObjectId(userId)){
+        throw new ApiError(400, "Invalid User id")
+    }
+
+    const existedLike = await Like.findOne({tweet: tweetId})
+
+    if(existedLike){
+        const like = await Like.findOneAndDelete(tweetId)
+        return res
+        .status(200)
+        .json( new ApiResponse(200, like, "Tweet unlike"))
+    }else{
+        const like = await Like.create({
+            likedBy: userId,
+            tweet: tweetId
+        })
+
+        return res
+        .status(200)
+        .json( new ApiResponse(200, like, "Tweet liked"))
+    }
 }
 )
 
--- src/routes/like.route.js
+++ src/routes/like.route.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
-import { toggleVideoLike } from "../controllers/like.controller.js";
+import { toggleCommentLike, toggleTweetLike, toggleVideoLike } from "../controllers/like.controller.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router()
 router.use(verifyJWT)
 router.route("/toggle/v/:videoId").post(toggleVideoLike)
+router.route("/toggle/c/:commentId").post(toggleCommentLike)
+router.route("/toggle/t/:tweetId").post(toggleTweetLike)
 
 
 export default router
